f47b42bd38bea666e4f42858a1ab5546bee7c79a
abinkhd <abinkhd@gmail.com>
2024-11-28 13:08:35 +0530

    Crud operations done - Implemented Edit, Mark as Completed, Delete - Implemented Create button and Create Task Form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
-// import Login from "./components/Login";
-import Navbar from "./components/Navbar";
 import { tasks, users } from "./data";
 import routes from "./routes/routes";
+import UserProvider from "./statemanagement/context/UserProvider";
 
 function App() {
   localStorage.setItem("users", JSON.stringify(users));
   localStorage.setItem("tasks", JSON.stringify(tasks));
-  return <RouterProvider router={routes} />;
+  return (
+    <UserProvider>
+      <RouterProvider router={routes} />
+    </UserProvider>
+  );
 }
 
 export default App;
